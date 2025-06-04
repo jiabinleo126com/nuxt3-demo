5e18be6fbfee5dd4d4012ecc65bc57424840f4cb
jiabinleo126com <jiabinleo@126.com>
2025-06-04 11:43:01 +0800

    香港访校营

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,6 +10,44 @@ export default defineNuxtConfig({
                     src: "https://www.ieduchina.com/statics/js/jquery-3.2.1.min.js",
                     type: "text/javascript",
                     defer: true
+                },
+                {
+                    src: "https://www.ieduchina.com/statics/pcc/js/swiper.min.js",
+                    type: "text/javascript",
+                    defer: true
+                }, {
+                    src: "https://cloudcache.tencentcs.com/qcloud/video/dist/tcadapter.1.0.0.min.js",
+                    type: "text/javascript",
+                    defer: true
+                }, {
+                    src: "https://web.sdk.qcloud.com/player/tcplayer/release/v4.5.4/libs/TXLivePlayer-1.2.3.min.js",
+                    type: "text/javascript",
+                    defer: true
+                }, {
+                    src: "https://web.sdk.qcloud.com/player/tcplayer/release/v4.5.4/libs/hls.min.1.1.5.js",
+                    type: "text/javascript",
+                    defer: true
+                }, {
+                    src: "https://web.sdk.qcloud.com/player/tcplayer/release/v4.5.4/libs/flv.min.1.6.3.js",
+                    type: "text/javascript",
+                    defer: true
+                }, {
+                    src: "https://web.sdk.qcloud.com/player/tcplayer/release/v4.5.4/libs/dash.all.min.4.4.1.js",
+                    type: "text/javascript",
+                    defer: true
+                }, {
+                    src: "https://web.sdk.qcloud.com/player/tcplayer/release/v4.5.4/tcplayer.v4.5.4.min.js",
+                    type: "text/javascript",
+                    defer: true
+                }
+            ],
+            link: [
+                {
+                    rel: "stylesheet",
+                    href: "https://www.ieduchina.com/statics/pc/css/swiper.min.css"
+                }, {
+                    rel: "stylesheet",
+                    href: "https://web.sdk.qcloud.com/player/tcplayer/release/v4.5.4/tcplayer.min.css"
                 }
             ]
         },
