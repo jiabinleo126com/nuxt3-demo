599aadece6cdaf21a7125abd3a4c8c76d7d2b93f
jiabinleo126com <jiabinleo@126.com>
2025-08-25 15:44:58 +0800

    添加升学备考子页面

---
+++ pages/topic/2025/shengxue/beikao.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { defineAsyncComponent, onMounted } from 'vue';
+const Header = defineAsyncComponent(() => import(`~/components/topic/2025/shengxue/Header.vue`))
+const Title = defineAsyncComponent(() => import(`~/components/topic/2025/shengxue/Title.vue`))
+const Footer = defineAsyncComponent(() => import(`~/components/topic/2025/shengxue/Footer.vue`))
+</script>
+<template>
+  <component :is="Header" />
+  <component :is="Footer" />
+</template>
+<style lang="less" scoped module></style>
